fa9741496e6945b26b259d84e7088dd8f4034c63
13Tiwari <arnavt@vt.edu>
2024-08-02 11:50:46 -0400

    Updates

--- firebase.js
+++ firebase.js
@@ -6,7 +6,8 @@ const firebaseConfig = {
  projectId: "pantrytracker-55862",
  storageBucket: "pantrytracker-55862.appspot.com",
  messagingSenderId: "224855247188",
- appId: "1:224855247188:web:931670722b6815806c60c1"
+ appId: "1:224855247188:web:931670722b6815806c60c1",
+ measurementId: "G-SBM7C8K403"
  };
 const app = initializeApp(firebaseConfig);
 const firestore = getFirestore(app);
