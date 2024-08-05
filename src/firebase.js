a9f437def9acf2e0488e20a03c3de7b0f2d6ae7f
13Tiwari <arnavt@vt.edu>
2024-08-04 22:58:22 -0400

    All changes to make app
    Updated interface

--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,7 @@
-import { initializeApp } from 'firebase/app';
-import { getFirestore } from 'firebase/firestore';
+import {initializeApp} from 'firebase/app'
+import {getAnalytics} from 'firebase/analytics'
+import {getFirestore} from 'firebase/firestore'
+
 const firebaseConfig = {
  apiKey: "AIzaSyBaPZ61XFDQPxV6-5HXCIegCDyzqNpX0us",
  authDomain: "pantrytracker-55862.firebaseapp.com",
@@ -9,6 +11,9 @@ const firebaseConfig = {
  appId: "1:224855247188:web:931670722b6815806c60c1",
  measurementId: "G-SBM7C8K403"
  };
-const app = initializeApp(firebaseConfig);
-const firestore = getFirestore(app);
-export { firestore };
+
+const app = initializeApp(firebaseConfig)
+const analytics = getAnalytics(app)
+const firestore = getFirestore(app)
+
+export {firestore}
